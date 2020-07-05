29218f64910b6cc554c3387ec2f1265e478fed7f
ezzamel <ezzamel156@gmail.com>
2020-07-05 18:29:15 +0800

    Done building recipe Model

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, loader } from './views/base'
 
 /** Global state of app
